729f0cfec9b99bca64c1f902a084a1157b572ccf
Alfredo Bautista <117385282+BauDevp@users.noreply.github.com>
2022-11-07 16:15:06 -0600

    .

---
+++ retiro.php
@@ -0,0 +1,9 @@
+<?php
+include "config.php";
+$inout = file_get_contents('php://input');
+$data = json_decode($input, true);
+$message = array();
+$cantidad = $data['cantidad'];
+$conceptoRetiro= $data['conceptoRetiro'];
+$tipoRetiro = $data['tipoRetiro'];
+
